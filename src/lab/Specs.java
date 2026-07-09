14b14d05f65f7945fd4bef307cf91c5903ec6feb
Pattamakorn49 <pattamakorn.j@ku.th>
2026-07-09 19:28:30 +0700

    Add files via upload

--- src/lab/Specs.java
+++ src/lab/Specs.java
@@ -17,6 +17,13 @@ package lab;
 public class Specs {
 
     // TODO ข้อ 4: เขียน JavaDoc ให้เมธอดนี้
+    /**
+     * หาข้อมูล key ที่ต้องการใน arr (โดย arr ต้องไม่เป็น null)
+     * @param arr ข้อมูลที่รับเข้ามา ( ต้องไม่เป็น null )
+     * @param key ข้อมูลที่ต้องการหา
+     * @return จำนวนข้อมูลที่หาเจอ
+     * @throws IllegalArgumentException ถ้า arr เป็น null
+     */
     public static int firstIndexOf(int[] arr, int key) {
         if (arr == null) throw new IllegalArgumentException("arr must not be null");
         for (int i = 0; i < arr.length; i++)
@@ -26,9 +33,10 @@ public class Specs {
 
     // TODO ข้อ 5: เขียน JavaDoc ให้เมธอดนี้
     /**
-     * 
-     * @param radius
-     * @return
+     * สูตรหา circleArea โดยที่ radius มีค่า >= 0
+     * @param radius รัศมี (ต้องมีค่า >= 0)
+     * @return สูตรหาพื้นที่วงกลม
+     * @throws IllegalArgumentException ถ้า radius มีค่าเป็น null
      */
     public static double circleArea(double radius) {
         if (radius < 0) throw new IllegalArgumentException("radius must be >= 0");
@@ -38,6 +46,11 @@ public class Specs {
     // TODO ข้อ 6: เขียน JavaDoc ให้เมธอดนี้
     //   (เมธอดนี้ตัดช่องว่างหน้า-หลัง แล้วแปลงเป็นตัวพิมพ์เล็ก;
     //    input null ให้คืน string ว่าง)
+    /**
+     * แปลง s ที่รับเข้ามาตัดช่องว่างหน้า-หลัง แล้วแปลงเป็นตัวพิมพ์เล็กโดย s ต้องไม่เป็น null (ถ้าเป็นให้คืนstring ว่าง)
+     * @param s ข้อมูลที่รับเข้ามา ( ต้องไม่เป็น null ถ้าเป็น null ให้คืน string ว่าง )
+     * @return ข้อมูลที่มีการตัดช่องว่างหน้า-หลัง แล้วแปลงเป็นตัวพิมพ์เล็ก
+     */
     public static String normalize(String s) {
         if (s == null) return "";
         return s.trim().toLowerCase();
